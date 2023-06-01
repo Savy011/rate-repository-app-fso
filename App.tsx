f5807beebbf132cfa0afb8f934fa86953ea7b8f2
Savy011 <106910589+Savy011@users.noreply.github.com>
2023-06-02 00:21:27 +0530

    Finished Ex 10.3

--- App.tsx
+++ App.tsx
@@ -1,22 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/components/Main";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<Main />
 	);
 }
 
-const white = '#fff'
-
-const styles = StyleSheet.create({
-	container: {
-		alignItems: 'center',
-		backgroundColor: white,
-		flex: 1,
-		justifyContent: 'center',
-	},
-});
