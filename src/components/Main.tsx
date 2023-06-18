8d6d49806d80adeffff96f238ff74bbc96b90ddf
Savy011 <106910589+Savy011@users.noreply.github.com>
2023-06-18 15:45:29 +0530

    Finished Ex 10.10

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 import { Navigate, Route, Routes } from 'react-router-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
@@ -6,24 +6,30 @@ import theme from '../theme';
 import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: theme.colors.mainBackground,
-		flexGrow: 1,
-		flexShrink: 1,
-	},
+  container: {
+    backgroundColor: theme.colors.mainBackground,
+    flexGrow: 1,
+    flexShrink: 1,
+    fontFamily:
+      Platform.OS === 'android'
+        ? 'Roboto'
+        : Platform.OS === 'ios'
+        ? 'Arial'
+        : 'System'
+  }
 });
 
 const Main = () => {
-	return (
-		<View style={styles.container} >
-			<AppBar />
-			<Routes>
-				<Route path="/" element={<RepositoryList />} />
-				<Route path="/signin" element={<SignIn />}/>
-				<Route path="*" element={<Navigate to="/" replace />} />
-			</Routes>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </View>
+  );
 };
 
 export default Main;
